25cc37bb5e13a6b7a1a2d5827681ccadec4c6bf7
jxmeslukin <jimmy.lukin@gmail.com>
2022-05-31 21:17:21 +1000

    json data conversion

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-//calling blockchain.com api to fetch real time info
+// calling blockchain.com api to fetch real time info
 
 let socket = new WebSocket('wss://ws.blockchain.info/inv');
 
@@ -12,6 +12,51 @@ socket.onopen = function() {
 socket.onmessage = function(e) {
     var response = JSON.parse(e.data);
     var tx = response.x.out
-    console.log(tx);
+    console.log(response);
+    jsonManipulate(response)
 }
 
+function jsonManipulate(data) {
+    var arr = data.x
+    var hash = arr.hash
+    var time = arr.time
+    var value = arr.out
+    var valLeng = value.length
+    var sum = 0;
+
+    for(i=0; i < valLeng; i++) {
+        sum += (value[i].value)
+    }
+    var btc = sum / 100000000
+    var valSum = value.reduce((total, n) => total + n, 0)
+    currencyConvert(btc);
+    timeConvert(time);
+}
+
+function timeConvert(time) {
+    var date = new Date(time * 1000);
+    var hours = date.getHours();
+    var mins = "0" + date.getMinutes();
+    var formattedTime = hours + ":" + mins.slice(-2)
+    console.log(formattedTime)
+}
+
+function currencyConvert(btc) {
+
+}
+
+let blockSocket = new WebSocket('wss://ws.blockchain.info/inv');
+
+blockSocket.onopen = function() {
+    //alert("[open] Connection Established");
+    blockSocket.send(JSON.stringify({
+        "op": "blocks_sub",
+        "op": "ping_block"
+    }))
+}
+
+blockSocket.onmessage = function(e) {
+    var blockResponse = JSON.parse(e.data);
+    var tx = blockResponse.op.out
+    
+}
